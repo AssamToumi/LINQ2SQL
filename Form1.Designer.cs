b070d23b812fe2d91cf423f4a1a156ec9e48d219
Assam Toumi <assamtomi@outlook.com>
2020-07-03 21:15:07 +0100

    done!

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace LINQ2SQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.codeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new LINQ2SQL.database1DataSet();
+            this.employeeTableAdapter = new LINQ2SQL.database1DataSetTableAdapters.EmployeeTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codeDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.adressDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.employeeBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(344, 183);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // codeDataGridViewTextBoxColumn
+            // 
+            this.codeDataGridViewTextBoxColumn.DataPropertyName = "Code";
+            this.codeDataGridViewTextBoxColumn.HeaderText = "Code";
+            this.codeDataGridViewTextBoxColumn.Name = "codeDataGridViewTextBoxColumn";
+            this.codeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // adressDataGridViewTextBoxColumn
+            // 
+            this.adressDataGridViewTextBoxColumn.DataPropertyName = "Adress";
+            this.adressDataGridViewTextBoxColumn.HeaderText = "Adress";
+            this.adressDataGridViewTextBoxColumn.Name = "adressDataGridViewTextBoxColumn";
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataMember = "Employee";
+            this.employeeBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeTableAdapter
+            // 
+            this.employeeTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(109, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 26);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 269);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+        private database1DataSetTableAdapters.EmployeeTableAdapter employeeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ2SQL
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(LINQ2SQL.Properties.Settings.Default.database1ConnectionString);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dc = new DataClasses1DataContext(con);
+
+            var selectQuery =
+                from a in dc.GetTable<Employee>()
+                select a;
+            dataGridView1.DataSource = selectQuery;
+
+            //// TODO: This line of code loads data into the 'database1DataSet.Employee' table. You can move, or remove it, as needed.
+            //this.employeeTableAdapter.Fill(this.database1DataSet.Employee);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //create the new employee
+            DataClasses1DataContext dc = new DataClasses1DataContext(con);
+            Employee NewEmp = new Employee();
+            NewEmp.Code = 5;
+            NewEmp.Name = "ddddd";
+            NewEmp.Adress = "ddddd";
+
+            //add the new employee to database
+            dc.Employees.InsertOnSubmit(NewEmp);
+
+            //save changes to database
+            dc.SubmitChanges();
+
+            //rebind datagridview to display the new employee
+            var selectQuery =
+                from a in dc.GetTable<Employee>()
+                select a;
+            dataGridView1.DataSource = selectQuery;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dc = new DataClasses1DataContext(con);
+
+            //get employee to update
+            Employee employee = dc.Employees.FirstOrDefault(emp => emp.Code.Equals("2"));
+
+            //update employee
+            employee.Name = "csharp";
+            employee.Adress = "csharp";
+
+            //Save changes to Database.
+            dc.SubmitChanges();
+
+            //rebind datagridview to display the new employee
+            var selectQuery =
+                from a in dc.GetTable<Employee>()
+                select a;
+            dataGridView1.DataSource = selectQuery;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dc = new DataClasses1DataContext(con);
+
+            //get the employee to delete
+            Employee DeleteEmployee = dc.Employees.FirstOrDefault(emp => emp.Code.Equals("5"));
+
+            //delete the employee
+            dc.Employees.DeleteOnSubmit(DeleteEmployee);
+
+            //save changes to database
+            dc.SubmitChanges();
+
+            //rebind datagridview to display the new employee
+            var selectQuery =
+                from a in dc.GetTable<Employee>()
+                select a;
+            dataGridView1.DataSource = selectQuery;
+        }
+    }
+}
